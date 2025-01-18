6d1d6ef1e23cf6ab8502ecb5a2e7ef1c9e3a1a1c
fromreassuringlyfawn <mrzygja@proton.me>
2025-01-18 12:38:17 +0100

    fixed json.go

--- json.go
+++ json.go
@@ -27,4 +27,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Critical error writing response: %s", err)
+	}
 }
